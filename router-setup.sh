f9f8674e5611f0c296ff81bea1fe5420552b56cc
Pongchai <pongchai.droiddev@gmail.com>
2022-05-27 23:29:37 +0700

    update script
    -Add new SNS
    -Add script install

---
+++ router-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+sudo apt update && apt install git
+curl -fsSL https://get.docker.com -o get-docker.sh
+udo sh get-docker.sh
+curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+cd /home/ubuntu/
+git clone https://github.com/connext/nxtp-router-docker-compose.git
+cd /home/ubuntu/nxtp-router-docker-compose
+git config --global --add safe.directory /home/ubuntu/nxtp-router-docker-compose
+git checkout amarok
+
+
+cp .env.example .env
+sed -i 's/latest/0.2.0-beta.8/g' .env
+cp config.example.json config.json
+cp key.example.yaml key.yaml
+sed -i 's/dkadkjasjdlkasdladadasda/'$1'/g' key.yaml
+
+docker-compose create
+docker-compose up -d
