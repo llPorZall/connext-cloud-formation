53ac5a039b6101073c426b0cd9db0ce9bcbd6f68
Pongchai <pongchai.droiddev@gmail.com>
2022-05-27 23:41:08 +0700

    fix bug install script

--- router-setup.sh
+++ router-setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && apt install git
 curl -fsSL https://get.docker.com -o get-docker.sh
-udo sh get-docker.sh
+sudo sh get-docker.sh
 curl -SL https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
